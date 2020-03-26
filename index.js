e63a9ec1f83e0b6f5db505fba79878ab12b26f22
Isa Oxenaar <isamoxenaar@gmail.com>
2020-03-26 19:10:13 +0100

    server commit, have to find the github repository

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+const port = 4001;
+const app = express();
+
+const server = http.createServer(app);
+const io = socketIO(server);
+const allCoordinates = {};
+
+io.on("connection", socket => {
+  console.log("New client connected", socket.id);
+
+  socket.on("add coordinates", coordinate => {
+    const now = Date.now();
+    console.log("this is now", now);
+    console.log("new coordinates are: ", coordinate);
+    console.log("socket id", socket.id);
+    allCoordinates[socket.id] = coordinate;
+    allCoordinates.time = now;
+    io.emit("all coordinates", allCoordinates);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
