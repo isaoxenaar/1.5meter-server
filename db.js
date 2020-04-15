5262e78f9ea21e1dbeb9e296acdaaa9f6dd94ec1
Isa Oxenaar <isamoxenaar@gmail.com>
2020-04-15 16:57:40 +0200

    Server, emptying database

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const databaseURL =
   process.env.DATABASE_URL ||
   "postgres://postgres:secret@localhost:5432/postgres";
 const db = new Sequelize(databaseURL);
-db.sync({ force: false })
+db.sync({ force: true })
   .then(() => console.log("db is synced"))
   .catch(console.error);
 module.exports = db;
